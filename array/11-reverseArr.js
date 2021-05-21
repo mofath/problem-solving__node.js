57164b2a287d82d4150c52a015f5f58a5585b7e2
mofath <mohamed.elgaedi@gmail.com>
2021-05-21 21:12:56 +0200

    [array]: added reverseArr

---
+++ array/11-reverseArr.js
@@ -0,0 +1,43 @@
+const array = [1, 2, 5, 10, 15, 20];
+
+
+// using `reverse` built in function
+const reverseArr_1 = (arr) => {
+   const reversedArr = [...arr];
+
+   return reversedArr.reverse();
+}
+
+// using `push` built in function
+const reverseArr_2 = (arr) => {
+   const reversedArr = [];
+
+   arr.map((_, index) =>
+      reversedArr.push(arr[arr.length - 1 - index])
+   );
+
+   return reversedArr
+}
+
+// using `unshift` built in function
+const reverseArr_3 = (arr) => {
+   const reversedArr = [];
+
+   arr.map((val) => {
+      reversedArr.unshift(val)
+   });
+
+   return reversedArr;
+};
+
+
+// using formula
+const reverseArr_4 = (arr) => {
+   return arr.map((_, index) => arr[(arr.length - 1 - index) % arr.length]);
+};
+
+
+console.log(reverseArr_1(array));
+console.log(reverseArr_2(array));
+console.log(reverseArr_3(array));
+console.log(reverseArr_4(array));
