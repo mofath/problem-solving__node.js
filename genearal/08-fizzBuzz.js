7e444a610ad5fec883d190cdf0b418ff31f60640
mofath <mohamed.elgaedi@gmail.com>
2021-06-05 15:14:28 +0200

    [general] added fizzBuzz

---
+++ genearal/08-fizzBuzz.js
@@ -0,0 +1,13 @@
+const n = 20;
+
+const fizzBuzz = (n) => {
+    for (let i = 0; i < n; i++) {
+        if (i % 15 === 0) console.log('FizzBuzz');
+        else if (i % 3 === 0) console.log('Fizz');
+        else if (i % 5 === 0) console.log('Buzz');
+        else console.log(i);
+    }
+
+}
+
+console.log(fizzBuzz(n));
