88ecefb4a2711b51864b93a6c88d91ebdd93981b
Mhmd Fathi <mohamed.elgaedi@gmail.com>
2021-09-16 23:09:48 +0200

    [string]: adjusted checkHTMLNesting

--- string/02-checkHTMLNesting.js
+++ string/02-checkHTMLNesting.js
@@ -1,55 +1,52 @@
-// const str = "<div><div><b></b></div></div>" // true
-// const str = "<i><div><b></b></div></i>"; // true
-// const str = "<div><div><b></b></div></p>" // div
+// const str = '<div><div><b></b></div></div>'; // true
+const str = '<div>abc</div><p><em><i>test test test</b></em></p>';
+// const str = '<i><div><b></b></div></i>'; // true
+// const str = '<p><div><b></b></div></p>'; // true
+// const str = '<p><div><b></b></div></p>'; // true
+// const str = '<div><div><b></b></div></p>'; // div
 // const str = `<div>abc </div><p><em><i>test test test</b></em></p>`; // i
-// const str = "<div><div><b></b></div></p>" // div
-// const str = "<p><div><b></b></div></p>"   // true
-const str = "<p><div>  </p></div>"            // p // failed
-// const str = "<p><div></b></a></div></p>" // b //failed
-// const str = "<p><div><b></b></div></p>"   // true
-
+// const str = '<div><div><b></b></div></p>'; // div
+// const str = '<p><div></b></a></div></p>'; // b
+// const str = '<p><div></p></div>'; // p // failed
+// const str = '<div>'; // div // failed
+// const str = '<p><p><em></em><p></p><i></p>'; // i // failed
 
 const checkHTMLNesting = (str) => {
+  const openingTags = str.match(/<\w+>/g);
 
-    const openingTags = str.match(/<\w+>/g);
+  const closingTags = str.match(/<\/\w+>/g);
 
-    const closingTags = str.match(/<\/\w+>/g);
+  const elemObj = {
+    '<b>': '</b>',
+    '<i>': '</i>',
+    '<em>': '</em>',
+    '<div>': '</div>',
+    '<p>': '</p>',
+  };
 
-    const elemObj = {
-        "<b>": "</b>",
-        "<i>": "</i>",
-        "<em>": "</em>",
-        "<div>": "</div>",
-        "<p>": "</p>",
-    };
+  let j = Math.min(openingTags.length, closingTags.length);
 
-    let i = 0;
-    let j = openingTags.length - 1;
-
-    while (j > 0) {
-
-        if (elemObj[openingTags[i]] === closingTags[i]) {
-            openingTags.shift();
-            closingTags.shift();
-            j--;
-        }
-
-        else if (elemObj[openingTags[i]] === closingTags[j]) {
-            openingTags.shift();
-            closingTags.pop();
-            j--;
-        }
-
-        else {
-            j = 0;
-        }
+  while (j > 0) {
+    if (elemObj[openingTags[0]] === closingTags[0]) {
+      openingTags.shift();
+      closingTags.shift();
+      j--;
+    } else if (
+      elemObj[openingTags[0]] === closingTags[closingTags.length - 1]
+    ) {
+      openingTags.shift();
+      closingTags.pop();
+      j--;
+    } else {
+      j = 0;
     }
+  }
 
-    return (elemObj[openingTags[i]] === closingTags[i]) ? true : openingTags[i].replace(/<|>/g, '');
-}
+  if (openingTags.length === 0 && closingTags.length === 0) return true;
+  if (openingTags.length === 0) return closingTags[0].replace(/<|>|\//g, '');
+  return openingTags[0].replace(/<|>/g, '');
+};
 
 console.log(checkHTMLNesting(str));
 
-
-
-
+4;
